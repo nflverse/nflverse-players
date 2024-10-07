955a09feaacff183251b8aafb02625f652d202e7
mrcaseb <mrcaseb@gmail.com>
2024-10-07 16:56:36 +0200

    automation scripts and workflow file

---
+++ build/00_update_roster_and_draft.R
@@ -0,0 +1,15 @@
+season <- Sys.getenv("NFLVERSE_UPDATE_SEASON", unset = NA_character_) |> as.integer()
+
+type <- Sys.getenv("NFLVERSE_UPDATE_TYPE", unset = NA_character_)
+type <- rlang::arg_match0(type, c("roster", "raw_draft"))
+
+if (type == "roster"){
+  out <- nflverse.players::.basis_release_raw_roster(seasons = season)
+} else if (type == "raw_draft"){
+  # We ask for Draft in every Workflow run but we actually try to scrape
+  # only if the system date is inside an arbitrarily chosen range around the
+  # possible draft date at the end of April
+  if (Sys.Date() %in% nflverse.players:::.draft_date_range()){
+    out <- nflverse.players::.draft_release_season(years = season)
+  }
+}
---
+++ build/01_update_components.R
@@ -0,0 +1,14 @@
+type <- Sys.getenv("NFLVERSE_UPDATE_TYPE", unset = NA_character_)
+type <- rlang::arg_match0(type, c("basis", "pfr", "otc", "draft"))
+
+if (type == "basis"){
+  out <- nflverse.players::players_basis_release()
+} else if (type == "pfr"){
+  # Uses env vars PLAYERS_PFR_REBUILD & PLAYERS_PFR_BASE
+  out <- nflverse.players::players_pfr_release()
+} else if (type == "otc"){
+  # Uses env vars PLAYERS_OTC_PLAYERID_ENDPOINT & PLAYERS_OTC_API_KEY
+  out <- nflverse.players::players_otc_release()
+} else if (type == "draft"){
+  out <- nflverse.players::players_draft_release()
+}
